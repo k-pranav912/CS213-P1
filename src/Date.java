a05dace17df17232241a591a57fd2ddb35c566f8
Saipranav Kalapala <sk2048@scarletmail.rutgers.edu>
2021-09-21 19:05:14 -0400

    added Date class methods

--- src/Date.java
+++ src/Date.java
@@ -3,7 +3,24 @@ public class Date implements Comparable<Date>{
     private int month;
     private int day;
 
-    public Date(String date) {} //take "mm/dd/yy" and create a Date object
+    //TODO add toString for date.
+
+    public Date(String date) {  //take "mm/dd/yy" and create a Date object
+
+        final int yearIndex = 6;
+        final int monthStartIndex = 0;
+        final int monthEndIndex = 2;
+        final int dayStartIndex = 3;
+        final int dayEndIndex = 5;
+
+        assert date.length() == "mm/dd/yy".length();
+
+        this.year = Integer.parseInt(date.substring(yearIndex));
+        this.month = Integer.parseInt(date.substring(monthStartIndex, monthEndIndex));
+        this.day = Integer.parseInt(date.substring(dayStartIndex, dayEndIndex));
+
+
+    }
     public Date() {} //create an object with today's date (see Calendar class)
 
     public boolean isValid() {
@@ -12,6 +29,11 @@ public class Date implements Comparable<Date>{
 
     @Override
     public int compareTo(Date date) {
+        if (this.year == date.month && this.month == date.month && this.day == date.day) {
+            return 0;
+        } else if () {
+
+        }
         return 0;
     }
 
