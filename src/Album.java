126d9416939077e29118e4491ec632f8e93a160a
nprabcode <nvp40@scarletmail.rutgers.edu>
2021-09-22 17:10:13 -0400

    Updated Album and Genre Classes

--- src/Album.java
+++ src/Album.java
@@ -1,12 +1,29 @@
+
+
 public class Album
 {
+    public static void main(String[] args)
+    {
+        Date hi = new Date();
+        Album newAlbum = new Album("I don't have one",
+                "Everyone", Genre.toGenre("Jazz"), hi, false);
+        System.out.println(newAlbum);
+    }
     private String title;
     private String artist;
     private Genre genre; //enum class; Classical, Country, Jazz, Pop, Unknown
     private Date releaseDate;
     private boolean isAvailable;
 
-    public Album() {}
+    public Album(String title, String artist, Genre genre, Date releaseDate, boolean isAvailable)
+    {
+        this.title = title;
+        this.artist = artist;
+        this.genre = genre;
+        this.releaseDate = releaseDate;
+        this.isAvailable = isAvailable;
+    }
+
 
     @Override
     public boolean equals(Object object)
@@ -22,15 +39,20 @@ public class Album
 
         return this.title.equals(obj.title) && this.artist.equals(obj.artist);
     }
-    @Override
-    public String toString()
+    public String availabilityToString()
     {
         String availability = "is not available";
         if (isAvailable)
         {
             availability = "is available";
         }
+        return availability;
+    }
 
-        return title + "::" + artist + "::" + genre + "::" + releaseDate.toString() + "::" + availability;
+    @Override
+    public String toString()
+    {
+        return title + "::" + artist + "::" + genre.toString()
+                + "::" + releaseDate.toString() + "::" + this.availabilityToString();
     }
 }
--- src/Genre.java
+++ src/Genre.java
@@ -1,4 +1,30 @@
 public enum Genre
 {
     CLASSICAL, COUNTRY, JAZZ, POP, UNKNOWN;
+
+
+    public static Genre toGenre(String genreString)
+    {
+        switch (genreString)
+        {
+            case "Classical": return CLASSICAL;
+            case "Country": return COUNTRY;
+            case "Jazz": return JAZZ;
+            case "Pop": return POP;
+            default: return UNKNOWN;
+        }
+    }
+
+    @Override
+    public String toString()
+    {
+        switch (this)
+        {
+            case CLASSICAL: return "Classical";
+            case COUNTRY: return "Country";
+            case JAZZ: return "Jazz";
+            case POP: return "Pop";
+            default: return "Unknown";
+        }
+    }
 }
