ead3d4ce132de6df5c3a965090ed5a2d9470489f
Saipranav Kalapala <sk2048@scarletmail.rutgers.edu>
2021-09-22 20:58:17 -0400

    modified Date class

--- src/Date.java
+++ src/Date.java
@@ -13,7 +13,7 @@ public class Date implements Comparable<Date>{
         final int dayStartIndex = 3;
         final int dayEndIndex = 5;
 
-        assert date.length() == "mm/dd/yy".length();
+        assert date.length() == "mm/dd/yyyy".length();
 
         this.year = Integer.parseInt(date.substring(yearIndex));
         this.month = Integer.parseInt(date.substring(monthStartIndex, monthEndIndex));
@@ -25,10 +25,31 @@ public class Date implements Comparable<Date>{
         Calendar calInstance = Calendar.getInstance();
         //System.out.println(calInstance.getTimeZone());
 
-        this.year =  calInstance.get(Calendar.YEAR)%1000;
-        //System.out.println(calInstance.get(Calendar.MONTH));
+        this.year =  calInstance.get(Calendar.YEAR);
         this.month = calInstance.get(Calendar.MONTH) + 1;
         this.day = calInstance.get(Calendar.DATE);
+    }
+
+    private static boolean isLeap(Date date) {
+
+        final int QUADRENNIAL = 4;
+        final int CENTENNIAL = 100;
+        final int QUATERCENTENNIAL = 400;
+        final int start = 0;
+
+        switch (start) {
+
+            case 0: if (!(date.year % QUADRENNIAL == 0)) return false;
+
+            case 1: if (!(date.year % CENTENNIAL == 0)) return true;
+
+            case 2: if (!(date.year % QUATERCENTENNIAL == 0)) {
+                        return false;
+                    } else return true;
+
+        }
+
+        return false;
     }
 
     public boolean isValid() {
@@ -37,13 +58,9 @@ public class Date implements Comparable<Date>{
 
     @Override
     public int compareTo(Date date) {
-        if (this.year == date.month) {
+        if (this.year == date.year) {
             if (this.month == date.month) {
-
                 return Integer.compare(this.day, date.day);
-
-            //} else if (this.month < date.month) {
-             //   return -1;
             } else {
                 return Integer.compare(this.month, date.month);
             }
@@ -57,12 +74,15 @@ public class Date implements Comparable<Date>{
     public String toString() {
         return String.format("%02d", this.month) + "/" +
                 String.format("%02d", this.day) + "/" +
-                String.format("%02d", this.year);
+                String.format("%04d", this.year);
     }
 
     public static void main(String[] args) {
-        Date x = new Date();
+        Date x = new Date("08/31/2010");
+        Date y = new Date ("08/31/2001");
         System.out.println(x);
+        System.out.println(y);
+        System.out.println(x.compareTo(y));
     }
 
 }
